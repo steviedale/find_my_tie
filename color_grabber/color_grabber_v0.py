6f0cdb5f0b30d0ed3a8ffa575db468568d3f89e5
Steven Dale <sdale4@asu.edu>
2024-08-21 00:47:35 -0600

    added more data, added new color_grabber idea (nn approach), other stuff

--- color_grabber/color_grabber_v0.py
+++ color_grabber/color_grabber_v0.py
@@ -23,29 +23,28 @@ def grab_colors(img, n_colors=8, weights=(5, 100, 100, 1, 1), resize=False, blur
         for _ in range(blur):
             img = cv2.GaussianBlur(img, (3, 3), 0)
 
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    img_lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
     clt = KMeans(n_clusters=n_colors)
 
     # Get positional row/col channels to append to the points
-    # Ex: [[0, 0, 0, 0], [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]]
-    row_channel = np.arange(img_hsv.shape[0]).reshape(-1, 1)
-    row_channel = np.repeat(row_channel, img_hsv.shape[1], axis=1)
-    col_channel = np.arange(img_hsv.shape[1]).reshape(1, -1)
-    col_channel = np.repeat(col_channel, img_hsv.shape[0], axis=0)
-    img_hsv = np.dstack((img_hsv, row_channel, col_channel))
+    row_channel = np.arange(img_lab.shape[0]).reshape(-1, 1)
+    row_channel = np.repeat(row_channel, img_lab.shape[1], axis=1)
+    col_channel = np.arange(img_lab.shape[1]).reshape(1, -1)
+    col_channel = np.repeat(col_channel, img_lab.shape[0], axis=0)
+    img_lab = np.dstack((img_lab, row_channel, col_channel))
 
-    points = img_hsv.reshape(-1, 5)
+    points = img_lab.reshape(-1, 5)
     
     # apply mask
     mask = get_segmentation_mask(img)
     mask = mask.reshape(-1)
     points = points[mask.astype(bool)]
 
-    print(f"before: {len(points)}")
+    # print(f"before: {len(points)}")
     # th = np.percentile(points[:, 0], 25)
     th = np.percentile(points[:, 0], 2)
     points = points[points[:, 0] > th]
-    print(f"after: {len(points)}")
+    # print(f"after: {len(points)}")
 
     points = points * np.array(weights)
     labels = clt.fit_predict(points)
